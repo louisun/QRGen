bf3f2cb1338cf06e3b2f221fe7dd2e4c4cbddf73
louisun <read_on@126.com>
2018-01-12 15:47:31 +0800

    add feature to read the code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # QRGen
 
 使用`zxing`库生成二维码 Demo
+
+**输出：**
+
+![](http://7xj74s.com1.z0.glb.clouddn.com/2018-01-12-15-43-54_r99.png)
+
+![](http://7xj74s.com1.z0.glb.clouddn.com/2018-01-12-15-44-48_r99.png)
--- src/main/java/QRCode.java
+++ src/main/java/QRCode.java
@@ -1,9 +1,13 @@
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -20,10 +24,29 @@ public class QRCode {
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
     }
 
+
+    private static String decodeQRCodeImage(File imgFile) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(imgFile);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("Can not doecode the image");
+            return null;
+        }
+
+
+    }
+
     public static void main(String[] args) {
         System.setProperty("java.specification.version","1.9"); // ZXing只能识别1.X这种形式的java版本
         try {
-            generateQRCodeImage("http://www.baidu.com", 1350, 1350, QR_CODE_IMAGE_PATH);
+            generateQRCodeImage("http://www.baidu.com", 350, 350, QR_CODE_IMAGE_PATH);
+            System.out.println("QR Code Generated!");
+            System.out.println("Now we're going to decode the image:");
+            System.out.println("Texts: "+ decodeQRCodeImage(new File(QR_CODE_IMAGE_PATH)));
         } catch (WriterException e) {
             System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
         } catch (IOException e) {
